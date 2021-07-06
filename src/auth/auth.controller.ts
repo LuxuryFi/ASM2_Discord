53e92b5dfde2a24ebd95a5baa704bfaf14cd39e2
Anhnq215 <anhnq2@vmodev.com>
2021-07-06 23:17:39 +0700

    Update: Api for get admin list

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,12 @@
 import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { resolveSoa } from 'dns';
+import { AdminService } from 'src/admin/admin.service';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService : AuthService){}
+    constructor(private readonly authService : AuthService,
+        private readonly adminService: AdminService){}
 
     @Post()
     async validateUser(@Body() body, @Res() res){
@@ -17,6 +19,17 @@ export class AuthController {
             console.log("validate")
             console.log(error);
         }
-
     }
+
+    @Get()
+    async getUserList(@Body() body, @Res() res){
+        try {
+            const admins = await this.adminService.findAll();
+            res.send(admins);
+        } catch (error) {
+            console.log("validate")
+            console.log(error);
+        }
+    }
+
 }
